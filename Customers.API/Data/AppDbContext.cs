a2a584be748875e31b9a775d0cfd13332f4c35f2
JindoKimKor <jindokim.kor@gmail.com>
2026-02-12 21:19:33 -0500

    Initial commit: CarRental solution with Customers.API
    - CarRental.slnx solution file
    - Customers.API project (Week 3: EF Core, DB-first approach)
    - .gitignore for .NET projects

---
+++ Customers.API/Data/AppDbContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Customers.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Customers.API.Data;
+
+public partial class AppDbContext : DbContext
+{
+    public AppDbContext()
+    {
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Customer> Customers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Server=localhost;Database=CustomerDB;Trusted_Connection=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Customer>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Customer__3214EC075A39FF18");
+
+            entity.ToTable("Customer");
+
+            entity.Property(e => e.Email).HasMaxLength(255);
+            entity.Property(e => e.FirstName).HasMaxLength(100);
+            entity.Property(e => e.LastName).HasMaxLength(100);
+            entity.Property(e => e.Phone).HasMaxLength(20);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Customers.API/Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Customers.API.Models;
+
+public partial class Customer
+{
+	public int Id { get; set; }
+
+	[Required]
+	[MaxLength(50)]
+	public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public string Phone { get; set; } = null!;
+	
+	[EmailAddress]
+	public string Email { get; set; } = null!;
+}
