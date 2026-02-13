cf377c68ed813cfbd36a9c5250c21e47fe8a704f
JindoKimKor <jindokim.kor@gmail.com>
2026-02-13 18:40:08 -0500

    feat(maintenance-api): Add repair history endpoint with fake data
    - Implement RepairHistoryDto, IRepairHistoryService, FakeRepairHistoryService
    - Add MaintenanceController: GET /api/maintenance/vehicles/{vehicleId}/repairs
    - Register DI and verify via Swagger (tested vehicleId: 101)

---
+++ Maintenance.WebAPI/Controllers/MaintenanceController.cs
@@ -0,0 +1,24 @@
+﻿using Maintenance.WebAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Maintenance.WebAPI.Controllers
+{
+	[ApiController]
+	[Route("api/maintenance")]
+	public class MaintenanceController : ControllerBase
+	{
+		private readonly IRepairHistoryService _service;
+
+		public MaintenanceController(IRepairHistoryService service)
+		{
+			_service = service;
+		}
+
+		[HttpGet("vehicles/{vehicleId}/repairs")]
+		public IActionResult GetRepairHistory(int vehicleId)
+		{
+			var history = _service.GetByVehicleId(vehicleId);
+			return Ok(history);
+		}
+	}
+}
--- Maintenance.WebAPI/Program.cs
+++ Maintenance.WebAPI/Program.cs
@@ -1,3 +1,5 @@
+using Maintenance.WebAPI.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 //builder.Services.AddOpenApi();
+
+builder.Services.AddScoped<IRepairHistoryService, FakeRepairHistoryService>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
