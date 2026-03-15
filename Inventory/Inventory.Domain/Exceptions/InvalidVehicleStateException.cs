32daa1cbc4dfa658b4911eee2d14eb1cc86a5167
JindoKimKor <jindokim.kor@gmail.com>
2026-03-14 21:08:23 -0400

    refactor: Remove old domain structure, update Exception to standard pattern
    Removed Common/, Entities/, ValueObjects/, Enums/ folders (replaced by SeedWork + InventoryAggregate).
    Updated InvalidVehicleStateException with standard 3-constructor pattern.

--- Inventory/Inventory.Domain/Exceptions/InvalidVehicleStateException.cs
+++ Inventory/Inventory.Domain/Exceptions/InvalidVehicleStateException.cs
@@ -1,7 +1,13 @@
-﻿namespace Inventory.Domain.Exceptions
+namespace Inventory.Domain.Exceptions
 {
 	public class InvalidVehicleStateException : Exception
 	{
-		public InvalidVehicleStateException(string message) : base(message) { }
+		public InvalidVehicleStateException() { }
+
+		public InvalidVehicleStateException(string message)
+			: base(message) { }
+
+		public InvalidVehicleStateException(string message, Exception innerException)
+			: base(message, innerException) { }
 	}
 }
